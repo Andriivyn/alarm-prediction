da422d5dd8477b36c2a9db602bdff76365dce1bc
av <av@ggr.group>
2023-04-26 18:15:35 +0300

    try fix error

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {BehaviorSubject, catchError, map, retry, throwError} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import axios from 'axios';
 import {Prediction} from "../placeholders/alarms-placeholder";
 
 @Injectable({
@@ -31,6 +32,19 @@ export class DataService {
         //             this.iswData$.next(Object.assign([], response));
         //         }
         //     });
+        // this.postData({regionName: 'all'}).then(response => {
+        //     console.log(response);
+        // }).catch(error => {console.error(error)})
+        // fetch('http://<public-ip>/api', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': 'Bearer <token>',
+        //         // Replace https with http in the 'Origin' header
+        //         'Origin': window.location.origin.replace(/^https:/, 'http:')
+        //     },
+        //     body: {}
+        // }
     }
 
     public getPrediction(payload?) {
@@ -58,7 +72,8 @@ export class DataService {
     }
 
     public post(url, params) {
-        const headers = {'content-type': 'application/json'}
+        const headers = {'content-type': 'application/json',
+            'Origin': window.location.origin.replace(/^https:/, 'http:')}
         return this.http.post(url, params, {headers: headers})
             .pipe(
                 // timeout(1500),
@@ -74,5 +89,12 @@ export class DataService {
                 catchError(error => {
                     return throwError(error);
                 }))
+    }
+    postData(data: any): Promise<any> {
+        return axios.post('http://ec2-35-156-144-101.eu-central-1.compute.amazonaws.com/predictions', data)
+            .then(response => response.data)
+            .catch(error => {
+                throw new Error(error.message);
+            });
     }
 }
